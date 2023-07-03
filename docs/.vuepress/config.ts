ec37a52be391ebd233c11efec95574677d5b12fa
haozg-666 <106981170+haozg-666@users.noreply.github.com>
2023-07-03 21:36:34 +0800

    配置base修改

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig, defaultTheme } from 'vuepress';
 import { searchPlugin } from '@vuepress/plugin-search'
 
 export default defineUserConfig({
-  base: '/blog/',
+  base: '/',
   lang: 'zh-CN',
   title: 'haozg`s blog',
   description: 'haozg`s blog',
